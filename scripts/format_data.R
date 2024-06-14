0ddea45d4098d17011751ee6f2631d35a97b3fe3
maxianren <maxianren@gmail.com>
2024-06-14 19:09:39 -0400

    improve heatmap input parse, tpm_extractor -> data_feature_extractor

--- scripts/format_data.R
+++ scripts/format_data.R
@@ -18,55 +18,96 @@ GeneDataProcessor <- R6Class("GeneDataProcessor",
                            get_file_paths = function() {
                              file_paths <- list.files(self$directory, self$pattern, full.names = TRUE)
                              return(file_paths)
+                           },
+                           parse_file_paths = function() {
+                             file_name_full <- self$get_file_paths()
+                             file_name <- basename(file_name_full)
+                             file_name_clean <- gsub(" .*", "", file_name)
+                             
+                             file_name_clean_cy <- file_name_clean
+                             
+                             sample_id <- stringr::str_extract(file_name_clean_cy, "(?<=_)\\d+(?=_)")
+                             file_name_clean_cy <- stringr::str_remove(file_name_clean_cy, "_\\d+")
+                             genotype <- stringr::str_extract(file_name_clean_cy, "(?<=_)\\w+(?=-)")
+                             file_name_clean_cy <- stringr::str_remove(file_name_clean_cy, "_\\w+")
+                             gender <- stringr::str_extract(file_name_clean_cy, "(?<=-)\\w(?=-)")
+                             file_name_clean_cy <- stringr::str_remove(file_name_clean_cy, "-\\w")
+                             treatment <- stringr::str_extract(file_name_clean_cy, "(?<=-).+")
+                             
+                             df <- data.frame(
+                               file_name_full = file_name_full,
+                               file_name_clean = file_name_clean,
+                               sample_id = sample_id,
+                               genotype = genotype,
+                               gender = gender,
+                               treatment = treatment,
+                               stringsAsFactors = FALSE
+                             ) 
+                             return(df)
                            }
                          )
 )
 
-TPMExtractor <- R6Class("TPMExtractor",
-                        inherit = GeneDataProcessor,
-                        
-                        public = list(
-                          extract_tpm_pivot = function(df) {
-                            file_paths <- self$get_file_paths()
-                            updated_df <- df
-                            
-                            for (file_path in file_paths) {
-                              data_df <- read_csv(file_path)
-                              file_name <- gsub(pattern = " \\(GE\\)\\.csv$", replacement = "", x = basename(file_path))
-                              file_name_column <- paste(file_name, "TPM", sep = "_")
-                              
-                              merged_df <- updated_df %>%
-                                left_join(data_df, by = c("Name" = "Name")) %>%
-                                dplyr::select(Name, TPM) %>%
-                                dplyr::rename(!!file_name_column := TPM)
-                              
-                              updated_df[[file_name_column]] <- merged_df[[file_name_column]]
-                              updated_df <- updated_df %>%
-                                filter(!if_any(ends_with("TPM"), is.na))
-                            }
-                            return(updated_df)
-                          },
-                          
-                          extract_tpm_unpivot = function(df_pivot) {
-                            df_unpivot <- df_pivot %>%
-                              pivot_longer(
-                                cols = ends_with("_TPM"),
-                                names_to = "Sample", 
-                                values_to = "TPM"
-                              ) %>%
-                              dplyr::mutate(
-                                Number = str_extract(Sample, "(?<=_)\\d+(?=_?)"),
-                                Sample = str_remove(Sample, "_TPM"),
-                                Sample = str_remove(Sample, "_\\d+_"),
-                                Gene = str_extract(Sample, "^[^\\-]+"),
-                                Sample = str_remove(Sample, "^[^\\-]+\\-"),
-                                Gender = str_sub(Sample, 1, 1),
-                                Treatment = str_sub(Sample, 3)
-                              ) %>%
-                              dplyr::select(-Sample)
-                            return(df_unpivot)
-                          }
-                        )
+GeneFeatureExtractor <- R6Class("GeneFeatureExtractor",
+                                inherit = GeneDataProcessor,
+                                
+                                public = list(
+                                  feature = NULL,
+                                  
+                                  initialize = function(directory, pattern, feature) {
+                                    super$initialize(directory, pattern)
+                                    self$feature <- feature
+                                  },
+                                  
+                                  extract_feature_pivot = function(df) {
+                                    file_paths <- self$get_file_paths()
+                                    updated_df <- df
+                                    
+                                    for (file_path in file_paths) {
+                                      data_df <- read_csv(file_path)
+                                      file_name <- gsub(pattern = " .*", replacement = "", x = basename(file_path))
+                                      file_name_column <- paste(file_name, self$feature, sep = "_")
+                                      
+                                      merged_df <- updated_df %>%
+                                        left_join(data_df, by = c("Name" = "Name")) %>%
+                                        dplyr::select(Name, self$feature) %>%
+                                        dplyr::rename(!!file_name_column := self$feature)
+                                      
+                                      updated_df[[file_name_column]] <- merged_df[[file_name_column]]
+                                      updated_df <- updated_df %>%
+                                        filter(!if_any(ends_with(paste0("_", self$feature)), is.na))
+                                    }
+                                    return(updated_df)
+                                  },
+                                  
+                                  extract_feature_unpivot = function(df_pivot) {
+                                    df_unpivot <- df_pivot %>%
+                                      pivot_longer(
+                                        cols = ends_with(paste0("_", self$feature)),
+                                        names_to = "Sample",
+                                        values_to = self$feature
+                                      ) %>%
+                                      dplyr::mutate(
+                                        Sample = str_remove(Sample, paste0("_", self$feature)),
+                                        Number = str_extract(Sample, "(?<=_)\\d+(?=_?)"),
+                                        Sample = str_remove(Sample, "_\\d+_"),
+                                        Gene = str_extract(Sample, "^[^\\-]+"),
+                                        Sample = str_remove(Sample, "^[^\\-]+\\-"),
+                                        Gender = str_sub(Sample, 1, 1),
+                                        Treatment = str_sub(Sample, 3)
+                                      ) %>%
+                                      dplyr::select(-Sample)
+                                    return(df_unpivot)
+                                  },
+                                  
+                                  get_heatmap_hierarchy = function(suffix = paste0("_", self$feature)) {
+                                    df <- self$parse_file_paths()
+                                    df$file_name_clean <- paste0(df$file_name_clean, suffix)
+                                    df <- df %>%
+                                      arrange(desc(treatment), desc(gender))
+                                    return(df)
+                                  }
+                                )
 )
 
 DifferentialExpressionDataProcessor <- R6Class("DifferentialExpressionDataProcessor",
@@ -115,30 +156,29 @@ DataProcessor <- R6Class("DataProcessor",
                       diff_expr_file = NULL,
                       gene_folder = NULL,
                       pattern = NULL,
-                      top_n = NULL,
-                      pivot = NULL,
+
                       
-                      initialize = function(diff_expr_file, gene_folder, pattern, top_n, pivot = TRUE) {
+                      initialize = function(diff_expr_file, gene_folder, pattern) {
                         self$diff_expr_file <- diff_expr_file
                         self$gene_folder <- gene_folder
                         self$pattern <- pattern
-                        self$top_n <- top_n
-                        self$pivot <- pivot
+
                       },
                       
-                      extract_diff_expr_tpm = function() {
+                      extract_top_n_feature = function(feature, top_n, pivot) {
                         diff_expr <- DifferentialExpressionDataProcessor$new(self$diff_expr_file)
-                        combined_df <- diff_expr$get_top_n_genes(self$top_n)
-                        tpm_processor <- TPMExtractor$new(self$gene_folder, self$pattern)
+                        gene_top_n <- diff_expr$get_top_n_genes(top_n)
+                        feature_processor <- GeneFeatureExtractor$new(self$gene_folder, self$pattern, feature)
                         
-                        gene_tpm_pivot <- tpm_processor$extract_tpm_pivot(combined_df)
-                        if (self$pivot == FALSE) {
-                          gene_tpm <- tpm_processor$extract_tpm_unpivot(gene_tpm_pivot)
+                        gene_feature_pivot <- feature_processor$extract_feature_pivot(gene_top_n)
+                        if (pivot == FALSE) {
+                          gene_feature <- feature_processor$extract_feature_unpivot(gene_feature_pivot)
                         } else {
-                          gene_tpm <- gene_tpm_pivot
+                          gene_feature <- gene_feature_pivot
                         }
                         
-                        return(gene_tpm)
+                        return(gene_feature)
                       }
                     )
 )
+
